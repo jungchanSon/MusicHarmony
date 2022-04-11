f3369be62f73e1fb8c1bccf3025e91b3123a8e91
ABCXZY <rnrmfjc@gmail.com>
2022-04-11 21:09:16 +0900

    비ãmedia stream 제어

--- webrtc/src/public/js/app.js
+++ webrtc/src/public/js/app.js
@@ -1,32 +1,70 @@
-const socket = new WebSocket(`ws://${window.location.host}`);
-socket.addEventListener("open", ()=> {
-    console.log("Connec");
-});
-socket.addEventListener("message",(msg) => {
-    console.log("New Msg: ", msg);
-});
-socket.addEventListener("close", () => {
-    console.log("discon");
-});
-setTimeout(() => {
-    socket.send("hello device");
-}, 1000);
-
-
+// const socket = new WebSocket(`ws://${window.location.host}`);
+// socket.addEventListener("open", ()=> {
+//     console.log("Connec");
+// });
+// socket.addEventListener("message",(msg) => {
+//     console.log("New Msg: ", msg);
+// });
+// socket.addEventListener("close", () => {
+//     console.log("discon");
+// });
+// setTimeout(() => {
+//     socket.send("hello device");
+// }, 1000);
 const myFace = document.getElementById("myFace");
+const muteBtn = document.getElementById("mute");
+const cameraBtn = document.getElementById("camera");
+const camerasSelect = document.getElementById("cameras");
 
 let myStream;
+let muted = false;
+let cameraOff = false;
+
+async function getCameras() {
+    try{
+        const devices = await navigator.mediaDevices.enumerateDevices();
+        const cameras = devices.filter(device => device.kind === "videoinput");
+        cameras.forEach(camera => {
+            const option = document.createElement("option");
+            option.value = camera.deviceId;
+            option.innerText = camera.label;
+            camerasSelect.appendChild(option);
+        })
+    }catch (e) {console.log(e)}
+}
 
 async function getMedia() {
     try {
-        mySteam = await navigator.mediaDevices.getUserMedia({
+        myStream = await navigator.mediaDevices.getUserMedia({
             audio:true,
             video:true,
         });
-        console.log(myStream);
+        myFace.srcObject = myStream;
+        await getCameras();
     } catch(e) {
         console.log(e)
     }
 }
 
+const handleMuteBtn = () => {
+    myStream.getAudioTracks().forEach((track) => (track.enabled = !track.enabled))
+    if(!muted) {
+        muteBtn.innderText = "Unmute";
+    } else {
+        muteBtn.innerText = "Mute";
+    }
+}
+const handleCameraBtn = () => {
+    myStream.getVideoTracks().forEach((track) => (track.enabled = !track.enabled ))
+    if(cameraOff) {
+        cameraBtn.innerText = "Turn Camera Off";
+        cameraOff = false;
+    } else{
+        cameraBtn.innerText = "Turn Camera On";
+        cameraOff = true;
+    }
+}
+
+muteBtn.addEventListener("click", handleMuteBtn);
+cameraBtn.addEventListener("click", handleCameraBtn);
 getMedia();
--- webrtc/src/server.js
+++ webrtc/src/server.js
@@ -2,27 +2,18 @@ import express from "express";
 import http from "http";
 import WebSocket from "ws";
 import {log} from "nodemon/lib/utils";
+import SocketIO from "socket.io";
+
 const app = express();
 
 app.set("view engine", "pug");
 app.set("views", __dirname +"/views")
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
- app.get("/*", (req, res) => res.redirect("/"))
-const server = http.createServer(app);
-const wss = new WebSocket.Server({ server })
+app.get("/*", (req, res) => res.redirect("/"))
 
-const sockets = [];
-
-wss.on("connection", (socket) =>{
-    sockets.push(socket);
-    console.log("conneted");
-    socket.on("close", () => console.log("Disconnect"));
-    socket.on("message", (msg)=>{
-        console.log(msg);
-    });
-    socket.send("hello!");
-})
+const httpServer = http.createServer(app);
+const wsServer = SocketIO(httpServer)
 
 const handleListen = () => {console.log("http://localhost:3000");}
-server.listen(3000, handleListen);
+httpServer.listen(3000, handleListen);
