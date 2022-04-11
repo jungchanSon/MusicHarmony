af52ee5f2964cfbfe8920cc0d95304e5b2f4df88
ABCXZY <rnrmfjc@gmail.com>
2022-04-11 19:07:05 +0900

    socketIO 설치

--- webrtc/src/server.js
+++ webrtc/src/server.js
@@ -3,7 +3,11 @@ import express from "express";
 const app = express();
 
 app.set("view engine", "pug");
-app.set("views", __dirname +"/src/views")
+app.set("views", __dirname +"/views")
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (req, res) => res.render("home"));
 
-const handleListen = () => console.log("http://localhost:3000");
+
+const handleListen = () => {console.log("http://localhost:3000");}
+
 app.listen(3000, handleListen);
