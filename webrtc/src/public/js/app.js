b8e124f2fd50fd418122ef5820e810b6b414c55d
ABCXZY <rnrmfjc@gmail.com>
2022-04-11 20:15:31 +0900

    ìwebsocket 연습

--- webrtc/src/public/js/app.js
+++ webrtc/src/public/js/app.js
@@ -1,1 +1,32 @@
-Hello
+const socket = new WebSocket(`ws://${window.location.host}`);
+socket.addEventListener("open", ()=> {
+    console.log("Connec");
+});
+socket.addEventListener("message",(msg) => {
+    console.log("New Msg: ", msg);
+});
+socket.addEventListener("close", () => {
+    console.log("discon");
+});
+setTimeout(() => {
+    socket.send("hello device");
+}, 1000);
+
+
+const myFace = document.getElementById("myFace");
+
+let myStream;
+
+async function getMedia() {
+    try {
+        mySteam = await navigator.mediaDevices.getUserMedia({
+            audio:true,
+            video:true,
+        });
+        console.log(myStream);
+    } catch(e) {
+        console.log(e)
+    }
+}
+
+getMedia();
--- webrtc/src/server.js
+++ webrtc/src/server.js
@@ -1,13 +1,28 @@
 import express from "express";
-
+import http from "http";
+import WebSocket from "ws";
+import {log} from "nodemon/lib/utils";
 const app = express();
 
 app.set("view engine", "pug");
 app.set("views", __dirname +"/views")
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
+ app.get("/*", (req, res) => res.redirect("/"))
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server })
 
+const sockets = [];
+
+wss.on("connection", (socket) =>{
+    sockets.push(socket);
+    console.log("conneted");
+    socket.on("close", () => console.log("Disconnect"));
+    socket.on("message", (msg)=>{
+        console.log(msg);
+    });
+    socket.send("hello!");
+})
 
 const handleListen = () => {console.log("http://localhost:3000");}
-
-app.listen(3000, handleListen);
+server.listen(3000, handleListen);
