47f875192b1d48cf1de6c45e4d7a6ac14a430e7d
ABCXZY <rnrmfjc@gmail.com>
2022-04-13 04:23:18 +0900

    feat(/): 간단한 구동확인 용 코드들
    - Component
    - Store

--- front2/src/pages/_app.js
+++ front2/src/pages/_app.js
@@ -1,4 +1,6 @@
 import '../../styles/globals.css'
+import tw from "tailwind-styled-components"
+import {Themeprovider} from
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
---
+++ front2/src/store/testStore.js
@@ -0,0 +1,7 @@
+import create from 'zustand';
+
+const tempStore = create(() => {
+    count : 0;
+})
+
+// 사용 예시
