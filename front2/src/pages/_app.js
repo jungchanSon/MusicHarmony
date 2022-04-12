6d6920f08710d95e10460b0aea53762bf72f5b69
ABCXZY <rnrmfjc@gmail.com>
2022-04-13 04:46:50 +0900

    feat(/styeld): Theme , GlobalStyle 형식만 임시로 만듬
    - tempTheme(steylse/Themes) : 임시 Theme 이후 수정해서 사용해도 됨.
    - GlobalStyle :
    글씨  폰트나, 바뀌지 않을 레이아웃 등 ...

--- front2/src/pages/_app.js
+++ front2/src/pages/_app.js
@@ -1,9 +1,16 @@
-import '../../styles/globals.css'
-import tw from "tailwind-styled-components"
-import {Themeprovider} from
+import '../../styles/globals'
+import {Themeprovider} from "styled-component"
+import GlobalStyle from "../../styles/globals";
+import tempTheme from "../../styles/Themes/theme";
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+      <Themeprovider theme={tempTheme}>
+        <GlobalStyle/>
+        <Component {...pageProps} />
+      </Themeprovider>
+  )
 }
 
 export default MyApp
---
+++ front2/styles/Themes/theme.jsx
@@ -0,0 +1,17 @@
+// 임시 테마
+
+
+const palette = {
+    black: "#000000",
+    red: "#F00",
+    blue: "#00F",
+    green: "#0F0",
+    white: "#FFF"
+};
+
+const tempTheme ={
+    ...palette,
+}
+
+
+export default tempTheme;
