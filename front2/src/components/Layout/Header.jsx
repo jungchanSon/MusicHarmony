1c7b8c7b13642eab68780c284aa9cf155621d07d
ABCXZY <rnrmfjc@gmail.com>
2022-04-23 21:33:07 +0900

    feature(page/footer): footer 간단히 만듬
    - footer, 전체적인 간단 디자인

--- front2/src/components/Layout/Header.jsx
+++ front2/src/components/Layout/Header.jsx
@@ -1,29 +1,38 @@
-import styled , {css}from "styled-components";
+import styled from "styled-components";
+import Nav from "./Nav";
+import React from "react";
 
 const Header = () => {
 
     return(
         <>
-            <HeaderBox>
-                <LogoBox src="img/header.png" />
-                <h1 className={"text-5xl"} >Music Harmony</h1>
-                <h1></h1>
-            </HeaderBox>
+                <HeaderBox className={"pt-4"}>
+                    <h1 className={"text-5xl"} >Music Harmony</h1>
+                    <h1></h1>
+                    <Nav/>
+                </HeaderBox>
         </>
     );
 }
-
+// const Container = styled.div`
+//     background: ${props => props.theme.layoutColors.nav};
+//   display: flex;
+//   align-items: center;
+//   justify-content: space-between;
+// `
 const HeaderBox = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    width: 100vw; 
-    height: 20vh;
-    background: ${props => props.theme.layoutColors.nav};
+  display:flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  height: 20vh;
+  background: ${props => props.theme.layoutColors.nav};
+
     font-family: 'Gamja Flower', cursive;
 `
 const LogoBox = styled.img`
-    width: 20%;
-    height: 100%;
+  width: 30%;
+    height: min-content;
 `
 export default Header;
